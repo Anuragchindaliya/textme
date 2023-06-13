9ce40a892d7b6d00fdb9367e1fc3d9fb08c0db41
Anuragchindaliya <ak1148383@gmail.com>
2023-06-13 22:21:08 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="bg-primary text-primary-foreground h-screen">
+      <h1 className="">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
