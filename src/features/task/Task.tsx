81f27a30cd0810e85880b3839cbd78d8e9f3ec5a
Anuragchindaliya <ak1148383@gmail.com>
2025-05-29 23:18:58 +0530

    add: task filter toolbar

--- src/features/task/Task.tsx
+++ src/features/task/Task.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import TaskList from "./components/TaskList"
 import TaskForm from "./components/TaskForm"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "@/app/store"
 import { addTask, loadTasks, reorderTasks, updateTaskStatus } from "./taskSlice"
+import { Input } from "@/components/ui/input"
+import { DataTableFacetedFilter } from "./components/data-table-faceted-filter"
+import { statuses } from "./utils"
 
 const Task = () => {
   const dispatch = useDispatch()
   const tasks = useSelector((state: RootState) => state.tasks)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [selectedStatus, setSelectedStatus] = useState<string[]>([])
+  // console.log({ searchTerm, selectedStatus, tasks })
 
   const handleAddTask = (title: string) => {
     dispatch(addTask({ title }))
@@ -36,11 +42,68 @@ const Task = () => {
     localStorage.setItem("tasks", JSON.stringify(tasks))
   }, [tasks])
 
+  const filtertedTask = tasks.filter((task) => {
+      if (searchTerm && selectedStatus.length > 0) {
+        return (
+          task.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+          task.status &&
+          selectedStatus.includes(task.status)
+        )
+      } else if (searchTerm) {
+        return task.title.toLowerCase().includes(searchTerm.toLowerCase())
+      } else if (selectedStatus.length > 0 && task.status) {
+        return selectedStatus.includes(task.status)
+      }
+      return true
+    })
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-4">Task Management</h1>
+      <p className="mb-6">
+        Manage your tasks efficiently with drag and drop functionality.
+      </p>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-1 items-center space-x-2 mb-4">
+          <Input
+            placeholder="Filter tasks..."
+            value={searchTerm}
+            onChange={(event) =>
+              setSearchTerm(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+          {/* {table.getColumn("status") && ( */}
+          <DataTableFacetedFilter
+            selectedValues={selectedStatus}
+            setSelectedStatus={setSelectedStatus}
+            tasks={tasks.filter((task) => task.title.toLowerCase().includes(searchTerm.toLowerCase()))}
+            title="Status"
+            options={statuses}
+          />
+          {/* )} */}
+          {/*{table.getColumn("priority") && (
+          <DataTableFacetedFilter
+            column={table.getColumn("priority")}
+            title="Priority"
+            options={priorities}
+          />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <X className="ml-2 h-4 w-4" />
+          </Button>
+        )} */}
+        </div>
+        {/* <DataTableViewOptions table={table} /> */}
+      </div>
       <TaskList
-        tasks={tasks}
+        tasks={filtertedTask}
         onDragEnd={handleDragEnd}
         onToggleStatus={handleToggleTaskStatus}
       />
---
+++ src/features/task/components/data-table-faceted-filter.tsx
@@ -0,0 +1,157 @@
+import * as React from "react"
+import { Check, LucideIcon, PlusCircle } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Separator } from "@/components/ui/separator"
+import { TaskType } from "../task.types"
+
+interface DataTableFacetedFilter {
+  selectedValues: string[],
+  setSelectedStatus: React.Dispatch<React.SetStateAction<string[]>>
+  tasks:TaskType[]
+  // column?: Column<TData, TValue>
+  title?: string
+  options: {
+    id:string;
+    label: string
+    value: string
+    icon?: LucideIcon
+  }[]
+}
+
+export function DataTableFacetedFilter<TData, TValue>({
+  tasks,
+  selectedValues,
+  setSelectedStatus,
+  title,
+  options,
+}: DataTableFacetedFilter) {
+  // console.log("Selected values:", tasks);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size="sm" className="h-8 border-dashed">
+          <PlusCircle className="mr-2 h-4 w-4" />
+          {title}
+          {selectedValues?.length > 0 && (
+            <>
+              <Separator orientation="vertical" className="mx-2 h-4" />
+              <Badge
+                variant="secondary"
+                className="rounded-sm px-1 font-normal lg:hidden"
+              >
+                {selectedValues.length}
+              </Badge>
+              <div className="hidden space-x-1 lg:flex">
+                {selectedValues.length > 2 ? (
+                  <Badge
+                    variant="secondary"
+                    className="rounded-sm px-1 font-normal"
+                  >
+                    {selectedValues.length} selected
+                  </Badge>
+                ) : (
+                  options
+                    .filter((option) => selectedValues.includes(option.id))
+                    .map((option) => (
+                      <Badge
+                        variant="secondary"
+                        key={option.id}
+                        className="rounded-sm px-1 font-normal"
+                      >
+                        {option.label}
+                      </Badge>
+                    ))
+                )}
+              </div>
+            </>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0" align="start">
+        <Command>
+          <CommandInput placeholder={title} />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup >
+              {options.map((option) => {
+                // const isSelected = selectedValues.includes(option.value)
+                const isSelected = selectedValues?.includes(option.id) 
+                return (
+                  <CommandItem
+                    key={option.id}
+                    onSelect={() => {
+                      
+                      if (isSelected) {
+                        setSelectedStatus(selectedValues.filter((op)=>op!=option.id))
+                      } else {
+                        setSelectedStatus([...selectedValues,option.id])
+                      }
+                      // const filterValues = Array.from(selectedValues)
+                      // console.log("Selected values after toggle:", isSelected,selectedValues)
+                      // column?.setFilterValue(
+                      //   filterValues.length ? filterValues : undefined,
+                      // )
+
+
+                    }}
+                  >
+                    <div
+                      className={cn(
+                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                        isSelected
+                          ? "bg-primary text-primary-foreground"
+                          : "opacity-50 [&_svg]:invisible",
+                      )}
+                    >
+                      <Check className={cn("h-4 w-4")} />
+                    </div>
+                    {option.icon && (
+                      <option.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+                    )}
+                    <span>{option.label}</span>
+                    {tasks?.length > 0 && (
+                      <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
+                        {tasks.filter((op)=>op.status===option.id).length}
+                      </span>
+                    )}
+                  </CommandItem>
+                )
+              })}
+            </CommandGroup>
+            {selectedValues.length > 0 && (
+              <>
+                <CommandSeparator />
+                <CommandGroup>
+                  <CommandItem
+                    // onSelect={() => column?.setFilterValue(undefined)}
+                    className="justify-center text-center"
+                  >
+                    Clear filters
+                  </CommandItem>
+                </CommandGroup>
+              </>
+            )}
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
